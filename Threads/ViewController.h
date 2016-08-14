8b01427cc9ec57a295a245db20dc0f9abdcd020b
EnzoFurmenti <ducheenzo@mail.ru>
2016-08-14 07:21:10 +0400

    init

---
+++ Threads/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Threads
+//
+//  Created by EnzoF on 14.08.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Threads/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Threads
+//
+//  Created by EnzoF on 14.08.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
